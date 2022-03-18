d6a76aa238f487af7769a6cbf39519bb193fcc4b
Gabriela SIlva <gabisantos.enf@gmail.com>
2022-03-18 17:38:42 +0100

    format files

--- js/app.js
+++ js/app.js
@@ -1,42 +1,24 @@
-/**
- *
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- *
- * Dependencies: None
- *
- * JS Version: ES2015/ES6
- *
- * JS Standard: ESlint
- *
- */
-
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
- */
-
 //Build scroll-up button:
 
-mybutton = document.getElementById('myBtn');
+const mybutton = document.getElementById('myBtn');
 
 // When the user scrolls down 30px from the top of the document, show the button
-window.onscroll = function () { scrollFunction() };
+window.onscroll = function () {
+  scrollFunction();
+};
 
-function scrollFunction() {
-    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
+function scrollFunction () {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    mybutton.style.display = 'block';
+  } else {
+    mybutton.style.display = 'none';
+  }
 }
 // call the function When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-};
+function topFunction () {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
 /**
  * Define Global Variables
@@ -75,15 +57,15 @@ buildNav();
 /* Whenever the target meets a threshold specified for the IntersectionObserver, the callback is invoked. 
 The callback receives a list of IntersectionObserverEntry objects and the observer: */
 const changeActiveSec = (entries, observer) => {
-    entries.forEach((entry) => {
-        // only do ActiveClass if the element is fully on screen
-        if (entry.isIntersecting && entry.intersectionRatio == 1) {
-            entry.target.classList.add('your-active-class');
-        } else {
-            entry.target.classList.remove('your-active-class');
-        }
-    });
-}
+  entries.forEach(entry => {
+    // only do ActiveClass if the element is fully on screen
+    if (entry.isIntersecting && entry.intersectionRatio == 1) {
+      entry.target.classList.add('your-active-class');
+    } else {
+      entry.target.classList.remove('your-active-class');
+    }
+  });
+};
 
 // creates the observer
 const options = {
@@ -101,5 +83,5 @@ sections.forEach(section => {
 });
 
 /**
- * End Main Functions 
+ * End Main Functions
  */
